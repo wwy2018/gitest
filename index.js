2eddabd6baf860ac4bae1dbbb1f9b510d4f9b98a
wangweiyi <wangweiyi@koolearn-inc.com>
2021-01-19 14:49:28 +0800

    test

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let a1 = 2
-let b333 = 3
-let c4 = 4
-let d = 6
-let f=9
-let g=10
-let h=11
-let i = 12
-let j=13
-let k=14
-let l=15
+let a1 = 44;
+let b333 = 3;
+let c4 = 4;
+let d = 6;
+let f = 9;
+let g = 10;
+let h = 11;
+let i = 12;
+let j = 13;
+let k = 14;
+let l = 15;
 
-let na=1
-let nb=2
+let na = 1;
+let nb = 2;
 
-let d1=1
-let d2=2
-let d3=3
+let d1 = 1;
+let d2 = 2;
+let d3 = 3;
 
-let i=0
+let i = 0;
